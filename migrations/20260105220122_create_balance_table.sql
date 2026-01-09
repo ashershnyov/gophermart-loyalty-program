962273e22ee1c838c9ae639e779414414b65417b
ashershnyov <antshersh@gmail.com>
2026-01-09 13:18:12 +0300

    sprint5: fixed migration

--- migrations/20260105220122_create_balance_table.sql
+++ migrations/20260105220122_create_balance_table.sql
@@ -10,9 +10,9 @@ CREATE TABLE gophermart.balance (
 CREATE TABLE gophermart.withdrawals (
     user_id BIGSERIAL NOT NULL REFERENCES gophermart.users(id) ON DELETE CASCADE,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    order_id TEXT REFERENCES gophermart.orders("number") ON DELETE NO ACTION,
+    order_id TEXT NOT NULL,
     amount NUMERIC(10, 2) NOT NULL DEFAULT 0.0
-)
+);
 -- +goose StatementEnd
 
 -- +goose Down
